ea13e34ca9e094b12877658c79fb24572275a434
Abraham Flaxman <abie@uw.edu>
2025-01-01 19:42:39 -0800

    Add a way to save the network for subsequent analysis

---
+++ src/vivarium_eye_vessels/components/observers.py
@@ -0,0 +1,17 @@
+import numpy as np, pandas as pd
+from vivarium import Component
+
+class SaveParticles(Component):
+    name = 'SaveImage'
+
+    @property
+    def columns_required(self):
+        return ['x', 'y', 'z', 'parent_id', 'frozen', 'depth']
+
+    def setup(self, builder):
+        self.seed = builder.configuration.randomness.random_seed
+
+    def on_simulation_end(self, event):
+        pop = self.population_view.get(event.index)
+        fname = (f'{self.seed}.csv.bz2')
+        pop.to_csv(fname)
